def864bea095006f9c690bb13c4d2b541855730b
Mirza Jijieshvili <mirzajijieshvili@gmail.com>
2022-10-20 20:05:47 +0400

    add functions

--- PoCTest.py
+++ PoCTest.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from functions import *
 
 ## qarva part
-
 qarvaDriver = webdriver.Chrome(PATH)
 # 1
 qarvaDriver.get(URL_tbilisi)
---
+++ functions.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from time import sleep
+from path import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+qarvaDriver = webdriver.Chrome(PATH)
+# driverGraf = webdriver.Chrome(PATH)
+
+
+# def switchServer():
+#     print("switch server:\n1)tbilisi\n2)dublin\n3)sydney\n4)roaming\n")
+#     x = int(input())
+#     if x == 1:
+#         URL = URL_tbilisi
+#
+#     elif x == 2:
+#         URL = URL_dublin
+#
+#     elif x == 3:
+#         URL = URL_sydney
+#
+#     elif x == 4:
+#         URL = URL_roaming
+#
+#     else:
+#         switchServer()
+#
+# def run_silk_info():
+#
+# def settings_click():
+#
+# def return_reference():
+#
+# def return_player_version():
+#
+# def run_grafana():
+#
+# def authorization_grafana():
+#
+# def filter_by_reference(return_reference()):
+#
+
+
+
+
+###############################################
+switchServer()
+
